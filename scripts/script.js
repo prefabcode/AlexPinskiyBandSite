24409d08319bdc97f1778d8889dccc2df05398fb
prefabcode <prefabcode@gmail.com>
2024-08-20 19:11:56 -0400

    work on burger menu

---
+++ scripts/script.js
@@ -0,0 +1,5 @@
+document.getElementById('burger-menu').addEventListener('click', function() {
+  debugger;
+  const mobileNav = document.getElementById('mobile-nav');
+  mobileNav.classList.toggle('open');
+});
