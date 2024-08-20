8494273959568098b92949137e7ffed89909a3bd
prefabcode <prefabcode@gmail.com>
2024-08-20 19:30:33 -0400

    finish responsiveness for reviews section

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,4 @@
 document.getElementById('burger-menu').addEventListener('click', function() {
-  debugger;
   const mobileNav = document.getElementById('mobile-nav');
   mobileNav.classList.toggle('open');
 });
